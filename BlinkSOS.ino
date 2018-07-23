6040bf839423f55173a2e74741363944ef56607c
bhodgs <41565294+bhodgs@users.noreply.github.com>
2018-07-23 16:40:30 +1000

    Add files via upload

---
+++ BlinkSOS.ino
@@ -0,0 +1,44 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+delay(500); // wait for half a second
+digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+delay(500); // wait for a second
+digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+delay(500); // wait for half a second
+digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+delay(500); // wait for a second
+digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+delay(500); // wait for half a second
+digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+delay(1500); // wait for a second
+digitalWrite(LED_BUILTIN, HIGH);
+delay(1500);
+digitalWrite(LED_BUILTIN, LOW);
+delay(500);
+digitalWrite(LED_BUILTIN, HIGH);
+delay(1500);
+digitalWrite(LED_BUILTIN, LOW);
+delay(500);
+digitalWrite(LED_BUILTIN, HIGH);
+delay(1500);
+digitalWrite(LED_BUILTIN, LOW);
+delay(1500);
+digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+delay(500); // wait for half a second
+digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+delay(500); // wait for a second
+digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+delay(500); // wait for half a second
+digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+delay(500); // wait for a second
+digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+delay(500); // wait for half a second
+digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+delay(3500); // wait for a second
+}
